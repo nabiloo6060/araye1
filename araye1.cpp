d9aa53472fb6ac8164faa8f223fd574cdff319f9
nabiloo6060 <118072654+nabiloo6060@users.noreply.github.com>
2022-12-03 19:59:55 +0330

    Add files via upload

---
+++ araye1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+main(){
+
+
+int num[1];
+int a,b,i;
+for(int i=0 ; i<1  ;i++){
+}
+
+cout <<"please enter one number:";
+cin>>a;
+cout <<"please enter one number :";
+cin>>b;
+if (a<b){
+	cout<<"min:";
+
+}
+else{
+	cout<<"max:";
+}
+}
+
